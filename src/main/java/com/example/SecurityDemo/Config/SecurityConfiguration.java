1566ebee5f238aa19f873b9a867f1f6d4066da3a
MohamedTaha <mohamedtaha.9718@gmail.com>
2024-01-29 13:49:09 +0200

    Final code update

--- src/main/java/com/example/SecurityDemo/Config/SecurityConfiguration.java
+++ src/main/java/com/example/SecurityDemo/Config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
